5015da167bdc6ace2743086f37938ea6ffb9f8de
leoj2eephp <leandro.j2ee@gmail.com>
2022-07-14 13:58:33 -0400

    Update!

--- models/Producto.php
+++ models/Producto.php
@@ -5,10 +5,14 @@ class Producto {
     public $id;
     public $nombre;
     public $stock;
+    public $tipo_producto_id;
+    public $tipoProducto;
 
     public static function getAll() {
         $c = new ConexionBD();
-        $rs = $c->bd->query("SELECT * FROM producto");
+        $rs = $c->bd->query("SELECT p.*, tp.id AS tipo_producto_id, tp.nombre AS tipo_producto_nombre
+                            FROM producto p
+                            INNER JOIN tipo_producto tp ON tp.id = p.tipo_producto_id");
         $productos = [];
 
         foreach ($rs as $fila) {
@@ -16,7 +20,13 @@ class Producto {
             $producto->id = $fila["id"];
             $producto->nombre = utf8_encode($fila["nombre"]);
             $producto->stock = $fila["stock"];
+            $producto->tipo_producto_id = $fila["tipo_producto_id"];
 
+            $tipoProducto = new TipoProducto();
+            $tipoProducto->id = $fila["tipo_producto_id"];
+            $tipoProducto->nombre = utf8_encode($fila["tipo_producto_nombre"]);
+
+            $producto->tipoProducto = $tipoProducto;
             $productos[] = $producto;
         }
         
